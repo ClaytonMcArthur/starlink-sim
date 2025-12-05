ca17340e0ae0a1ec844d716a7131e7b611f0600e
ClaytonMcArthur <claxx0506@icloud.com>
2025-12-05 09:32:55 +0000

    Adding basic implementation to ground_station in sim-core

--- sim-core/ground_station.cpp
+++ sim-core/ground_station.cpp
@@ -1,1 +1,16 @@
 //sim-core/ground_station.cpp
+
+#include "ground_station.h"
+#include <cmath>
+
+Vec3 GroundStation::position() const {
+    double R = 6371.0;
+    double lat = deg2rad(latDeg);
+    double lon = deg2rad(lonDeg);
+
+    Vec3 v;
+    v.x = R * std::cos(lat) * std::cos(lon);
+    v.y = R * std::cos(lat) * std::sin(lon);
+    v.z = R * std::sin(lat);
+    return v;
+}
--- sim-core/ground_station.h
+++ sim-core/ground_station.h
@@ -1,1 +1,17 @@
 //sim-core/ground_station.h
+
+#pragma once
+
+#include "vec3.h"
+
+class GroundStation {
+    public:
+        int id{0};
+        double latDeg{0.0};
+        double lonDeg{0.0};
+
+        Vec3 position() const;
+
+    private:
+        double deg2rad(double d) const { return d * 3.141592653589793 / 180.0; }
+};
